8a149be1164ed8ef957bcd6a24891a6839b3f974
abroudoux <arthur.broudoux@gmail.com>
2023-10-23 22:37:08 +0200

    fix

--- index.js
+++ index.js
@@ -7,21 +7,3 @@ exec("bash scripts/play.sh", (error, stdout, stderr) => {
   }
   console.log(stdout);
 });
-
-
-// const get = require("./src/get.js");
-// const read = require("./src/read.js");
-// const play = require("./src/play.js");
-
-// async function playScripts() {
-//     try {
-//         await get();
-//         await read();
-//         await play();
-//     } catch (err) {
-//         console.error(err);
-//         process.exit(1); 
-//     }
-// };
-
-// playScripts();
