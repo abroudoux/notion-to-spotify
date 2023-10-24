0f239a0d33b087716eed3b790c3196e7e808ff7c
abroudoux <arthur.broudoux@gmail.com>
2023-10-24 10:11:16 +0200

    separate read and write

--- src/read/read.js
+++ src/read/read.js
@@ -1,28 +1,20 @@
-import { readFile, writeFile } from 'fs';
+import { readFile } from 'fs';
+import { write } from "../write/write.js"
 
 
 async function read() {
-
     readFile('exports/notion-export.json', 'utf8', (err, data) => {
         try {
             const jsonContent = JSON.parse(data);
             const filteredData = jsonContent.filter(item => item.properties.Listened.checkbox === false);
             const albumsText = filteredData.map(item => `${item.properties.Title.title[0].plain_text} - ${item.properties.Artist.rich_text[0].plain_text}`).join('\n');
-
-            writeFile('albums.txt', albumsText, 'utf8', (err) => {
-                if (err) {
-                    console.error("Can't write the file : ", err);
-                    return;
-                };
-                console.log("File writted successfully");
-            });
+            write(albumsText);
         } catch (err) {
             console.error("Can't read the file : ", err);
             return;
         };
     });
-
 };
 
-
 export { read };
+
