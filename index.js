c27b77a2d90e6ab89f9994941491ce53b1489b7d
arthur <arthur.broudoux@gmail.com>
2024-03-27 17:28:34 +0100

    get all the todos from a page, display status

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-import { get } from "./src/get/get.js";
-import { read } from "./src/read/read.js";
-import { play } from "./src/play/play.js";
+import { Client, iteratePaginatedAPI } from "@notionhq/client";
+import { config } from "dotenv";
 
+config();
 
-async function playScripts() {
-    try {
-        await get();
-        await read();
-        await play();
-    } catch (err) {
-        console.log(err);
-    };
+const pageId = process.env.NOTION_PAGE_ID;
+const apiKey = process.env.NOTION_API_KEY;
+
+const notion = new Client({ auth: apiKey });
+
+const getTextFromToDoBlock = (block) => {
+  if (block.type !== "to_do") return null;
+  const todoText = block.to_do.rich_text.map((t) => t.plain_text).join("");
+  return `${todoText} ${block.to_do.checked ? "(Checked)" : "(Unchecked)"}`;
 };
 
-playScripts().catch((err) => {
-    console.error(err);
-});
+async function retrieveToDoBlocks(id) {
+  console.log("Retrieving ToDo blocks (async)...");
+  const todoBlocks = [];
+  for await (const block of iteratePaginatedAPI(notion.blocks.children.list, {
+    block_id: id,
+  })) {
+    const text = getTextFromToDoBlock(block);
+    if (text !== null) todoBlocks.push(text);
+  }
+  return todoBlocks;
+}
+
+const printToDoBlocks = async () => {
+  const todoBlocks = await retrieveToDoBlocks(pageId);
+  console.log("Displaying ToDo blocks:");
+  for (const block of todoBlocks) console.log(block);
+};
+
+printToDoBlocks();
