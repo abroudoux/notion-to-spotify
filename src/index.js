daed6dca92bf0f9c867d99a8e3305076bb4f3edf
arthur <arthur.broudoux@gmail.com>
2024-03-27 22:58:31 +0100

    launch randomly an album on spotify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Client, iteratePaginatedAPI } from "@notionhq/client";
 import { config } from "dotenv";
 import { promises as fsPromises } from "fs";
+import { exec } from "child_process";
 
 config();
 
@@ -12,11 +13,11 @@ const notion = new Client({ auth: apiKey });
 const getTextFromToDoBlock = (block) => {
   if (block.type !== "to_do") return null;
   const todoText = block.to_do.rich_text.map((t) => t.plain_text).join("");
-  return `${todoText} ${block.to_do.checked ? "(Checked)" : "(Unchecked)"}`;
+  return block.to_do.checked ? null : todoText;
 };
 
 async function retrieveToDoBlocks(id) {
-  console.log("Retrieving ToDo blocks (async)...");
+  console.log("Retrieving Unchecked ToDo blocks...");
   const todoBlocks = [];
   for await (const block of iteratePaginatedAPI(notion.blocks.children.list, {
     block_id: id,
@@ -30,29 +31,34 @@ async function retrieveToDoBlocks(id) {
 const saveToDoBlocksUncheckedJson = async () => {
   const todoBlocks = await retrieveToDoBlocks(pageId);
   console.log("Saving unchecked ToDo blocks to file...");
-  const uncheckedBlocks = todoBlocks.filter(
-    (block) => !block.includes("(Checked)")
-  );
-  const modifiedBlocks = uncheckedBlocks.map((block) =>
-    block.replace(" (Unchecked)", "")
-  );
   await fsPromises.writeFile(
     "albums.json",
-    JSON.stringify(modifiedBlocks, null, 2)
+    JSON.stringify(todoBlocks, null, 2)
   );
 };
 
 const selectRandomAlbum = async () => {
+  console.log("Selecting random album...");
   const content = await fsPromises.readFile("albums.json", "utf-8");
   const albums = JSON.parse(content);
   const randomAlbumNumber = Math.floor(Math.random() * albums.length);
   const randomAlbum = albums[randomAlbumNumber];
-  console.log(randomAlbum);
+  console.log("Random album is: ", randomAlbum);
+  return randomAlbum;
+};
+
+const playRandomAlbum = async () => {
+  console.log("Playing random album...");
+  const randomAlbum = await selectRandomAlbum();
+  const albumName = randomAlbum.replace(/.* - /, "");
+  // console.log("Album Name : ", albumName);
+  exec(`spotify play album ${albumName}`);
 };
 
 const main = async () => {
   await saveToDoBlocksUncheckedJson();
   await selectRandomAlbum();
+  await playRandomAlbum();
 };
 
 main();
